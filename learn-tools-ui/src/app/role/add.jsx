0766a8c4300c1554d66fc5d943f9c3964d0af154
yobbo <yobbo_wang@163.com>
2018-09-05 13:45:44 +0800

    部分修改

--- learn-tools-ui/src/app/role/add.jsx
+++ learn-tools-ui/src/app/role/add.jsx
@@ -1,42 +1,22 @@
 'use strict';
 import React from 'react';
 import {Button, Checkbox, Dialog, Form, Input} from 'element-react'
-import {fetchMenuList} from "../../store/menu/action";
 
 class Add extends React.Component{
     constructor(props){
         super(props);
         this.state = {
+            dialogVisible: false,
+            saving: false,
             form: {
-                name: '',
-                authMenu: []
+                name: ''
             },
             rules: {
                 name: [
                     { required: true, message: '请输入角色名', trigger: 'blur' }
-                ],
-                authMenu: [
-                    { type: 'array', required: true, message: '请至少选择一个功能权限', trigger: 'change' }
                 ]
             },
-            checkAll: false,
-            isIndeterminate: true,
-            menus: ['用户管理', '角色管理', '菜单管理'],
-            checkedCities: [],
-            menuList: [],
         };
-        this.options = {
-            label: 'name',
-            children: 'zones'
-        }
-    }
-
-    componentDidMount(){
-        fetchMenuList().then(() => {
-
-        }).catch((e) => {
-            
-        });
     }
 
     onClose() {
@@ -49,14 +29,63 @@ class Add extends React.Component{
         });
     }
 
-    handleCheckedMenusChange(value) {
-        const checkedCount = value.length;
-        const citiesLength = this.state.menus.length;
+    handleCheckAllChange(index, checked){
+        if(this.props.menuList[index].children.length == 0){
+            this.setState({
+                ['isIndeterminate'+index]: false,
+                ['check'+index]: checked,
+                ['checkAuth'+index]: [].push(this.props.menuList[index].text)
+            })
+        }else{
+            let _c_ = [];
+            for(let i=0;i<this.props.menuList[index].children.length;i++){
+                _c_.push(this.props.menuList[index].children[i].text);
+            }
+            const checkAuth = checked ? _c_ : [];
+            this.setState({
+                ['isIndeterminate'+index]: false,
+                ['check'+index]: checked,
+                ['checkAuth'+index]: checkAuth
+            })
+        }
+    }
 
+    handleCheckedAuthChange(index, value){
+        const checkCount = value.length;
+        const authLength = this.props.menuList[index].children.length;
         this.setState({
-            checkedCities: value,
-            checkAll: checkedCount === citiesLength,
-            isIndeterminate: checkedCount > 0 && checkedCount < citiesLength,
+            ['checkAuth'+index]: value,
+            ['check'+index]: checkCount === authLength,
+            ['isIndeterminate'+index]: checkCount > 0 && checkCount < authLength,
+        });
+    }
+
+    save(e){
+        e.preventDefault();
+        this.refs.form.validate((valid) => {
+            if (valid) {
+                let i = 0, menuAuth = [];
+                while (this.state['checkAuth' + i]){
+                    if(this.props.menuList[i].children.length == 0 && this.props.menuList[i].text == this.state['checkAuth' + i][0]) {
+                        menuAuth.push(this.props.menuList[i].id);
+                        continue;
+                    }
+                    menuAuth.push(this.props.menuList[i].id);
+                    for(let k=0;k<this.state['checkAuth' + i].length;k++){
+                        for(let j=0;j<this.props.menuList[i].children.length;j++){
+                            if(this.props.menuList[i].children[j].text == this.state['checkAuth' + i][k]){
+                                menuAuth.push(this.props.menuList[i].children[j].id); //添加子功能菜单id
+                            }
+                        }
+                    }
+                    i++;
+                }
+                let params = {
+                    name: this.state.form.name,
+                    auths: menuAuth
+                };
+                //TODO 保存
+            }
         });
     }
 
@@ -69,50 +98,46 @@ class Add extends React.Component{
                 onClose={() => this.onClose.bind(this)}
             >
                 <Dialog.Body>
-                    <Form model={this.state.form} labelPosition={'left'} labelWidth={"80%"} rules={this.state.rules}>
+                    <Form model={this.state.form} ref="form" labelPosition={'left'} labelWidth={"80%"} rules={this.state.rules}>
                         <Form.Item label="角色名" prop="name">
                             <Input value={this.state.form.name} onChange={this.onChange.bind(this, 'name')} placeholder="角色名有中文、数组、英文组成且长度不超过15"/>
                         </Form.Item>
-                        <Form.Item label="功能权限" prop="authMenu" >
-                            <div style={{border: 'solid 1px #eaeefb', borderRadius: 4, padding: 5, marginBottom: 5}}>
-                                <Checkbox
-                                    checked={this.state.checkAll}
-                                    indeterminate={this.state.isIndeterminate}
-                                    onChange={()=>{}}>系统管理</Checkbox>
-                                <Checkbox.Group
-                                    value={this.state.menus}
-                                    onChange={this.handleCheckedMenusChange.bind(this)}>
-                                    {
-                                        this.state.menus.map((item, index) =>
-                                            <Checkbox key={index} label={item}></Checkbox>
-                                        )
-                                    }
-                                </Checkbox.Group>
-                            </div>
-
-                            <div style={{border: 'solid 1px #eaeefb', borderRadius: 4, padding: 5, marginBottom: 5}}>
-                                <Checkbox
-                                    checked={this.state.checkAll}
-                                    indeterminate={this.state.isIndeterminate}
-                                    onChange={()=>{}}>系统管理</Checkbox>
-                                <Checkbox.Group
-                                    value={this.state.menus}
-                                    onChange={this.handleCheckedMenusChange.bind(this)}>
-                                    {
-                                        this.state.menus.map((item, index) =>
-                                            <Checkbox key={index} label={item}></Checkbox>
-                                        )
-                                    }
-                                </Checkbox.Group>
-                            </div>
-
+                        <Form.Item label="功能权限" >
+                            {
+                                this.props.menuList.map((item, index) => {
+                                    return (
+                                        <div  key={index} style={{border: 'solid 1px #eaeefb', borderRadius: 4, padding: 5, marginBottom: 5}}>
+                                            <Checkbox checked={this.state['check'+index]}
+                                                      indeterminate={this.state['isIndeterminate'+index]}
+                                                      onChange={this.handleCheckAllChange.bind(this, index)}
+                                            >
+                                                {item.text}
+                                            </Checkbox>
+                                            {
+                                                item.children.length > 0 ?
+                                                <Checkbox.Group
+                                                    value={this.state['checkAuth'+index]}
+                                                    onChange={this.handleCheckedAuthChange.bind(this, index)}
+                                                >
+                                                    {
+                                                        item.children.map((item_c, index_c) => {
+                                                            return <Checkbox key={index_c} label={item_c.text} />
+                                                        })
+                                                    }
+                                                </Checkbox.Group>
+                                                : null
+                                            }
+                                        </div>
+                                    )
+                                })
+                            }
                         </Form.Item>
                     </Form>
                 </Dialog.Body>
 
                 <Dialog.Footer className="dialog-footer">
                     <Button onClick={ this.onClose.bind(this) }>取 消</Button>
-                    <Button type="primary" onClick={ this.onClose.bind(this) }>确 定</Button>
+                    <Button type="primary" onClick={ this.save.bind(this) } loading={this.state.saving}>{ this.state.saving ? '保存中...' : '确 定'}</Button>
                 </Dialog.Footer>
             </Dialog>
         )
--- learn-tools-ui/src/app/role/body.jsx
+++ learn-tools-ui/src/app/role/body.jsx
@@ -4,12 +4,14 @@ import {Table, Button, Loading} from 'element-react'
 import Add from './add';
 import {requestRoleData} from "../../store/role/action";
 import {connect} from "react-redux";
+import {fetchMenuList} from "../../store/menu/action";
 
 class Body extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             dialogVisible: false,
+            menuList: [],
             columns: [
                 {label: "角色名", prop: "name", width: 180},
                 {label: "用户状态", prop: "status", width: 150, render: (data) => {
@@ -41,6 +43,13 @@ class Body extends React.Component {
         this.props.requestRoleData("roleList").then(() => {
             this.setState({ loading : false });
         });
+        //查询菜单
+        fetchMenuList().then((result) => {
+            this.setState({ menuList: result })
+        }).catch((e) => {
+            this.setState({ menuList: [] })
+            console.log(e)
+        });
     }
 
     // child component callback change state. and close Dialog
@@ -56,7 +65,7 @@ class Body extends React.Component {
                 <Loading text="拼命加载中" loading = {this.state.loading}>
                     <div className={"body-child"}>
                         <Button type="success" icon="plus" onClick={ () => {this.setState({ dialogVisible: true })}} >添加角色</Button>
-                        {<Add dialogVisible = {this.state.dialogVisible} callback = {this.callback.bind(this)} />}
+                        {<Add dialogVisible = {this.state.dialogVisible} callback = {this.callback.bind(this)} menuList={this.state.menuList} />}
                     </div>
                     <Table
                         style={{width: '100%'}}
