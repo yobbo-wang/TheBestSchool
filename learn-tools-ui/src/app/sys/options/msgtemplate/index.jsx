0b479b4094bcf9fda7d1c1451080b5d185565a00
yobbo <yobbo_wang@163.com>
2018-09-21 18:05:01 +0800

    部分修改

--- learn-tools-ui/src/app/sys/options/msgtemplate/index.jsx
+++ learn-tools-ui/src/app/sys/options/msgtemplate/index.jsx
@@ -2,13 +2,13 @@
 import React from 'react';
 
 /**
- * 系统相关配置组件
+ * 系统消息模板配置组件
  * <P>
- *     域名、数据字典配置等
+ *
  */
 class SystemOptions extends React.Component{
     render(){
-        return <P></P>
+        return <div></div>
     }
 }
 
